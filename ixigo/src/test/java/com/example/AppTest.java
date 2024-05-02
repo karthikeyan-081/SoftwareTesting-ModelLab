05d8045998e12825fe8d3fe9ce49430ae9eae245
karthikeyan-081 <149998557+karthikeyan-081@users.noreply.github.com>
2024-05-02 11:19:36 +0530

    Add files via upload

---
+++ ixigo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class AppTest 
+{
+    WebDriver driver;
+   
+    @BeforeTest
+    void beforeTest()
+    {
+       
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.ixigo.com/");
+    }
+    @Test
+    void Test1()
+    {
+        SoftAssert assert1=new SoftAssert();
+       
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[1]/div[1]/div/button[2]")).click();;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div/div/div/p[2]")).click();;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/div[2]/div/div/div[2]/input")).sendKeys("IDP-Independence");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/div[3]/div[1]/li/div[2]/p[1]/span[1]")).click();
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div")).click();;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/div[2]/div/div/div[2]/input")).sendKeys("DEL-NEW Delhi");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[1]/div[2]/div[3]/div[1]/li/div[2]/p[1]/span[1]")).click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[2]/div[1]/div/div/div/div/p[2]")).click();;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[2]/div[3]/div/div[1]/div[2]/div[1]/div/div/div[2]/button[28]/abbr")).click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[2]/div[2]/div/div[1]/div/div/p[2]")).click();;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[2]/div[3]/div/div[1]/div[2]/div[1]/div/div/div[2]/button[31]/abbr")).click();
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[3]/div[1]/div/div/div/p[2]")).click();;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[1]/div[1]/div[2]/div/button[2]")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[1]/div[2]/div[2]/div/button[3]")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[1]/div[5]/div/div[3]")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/div[3]/div[2]/button")).click();
+    }
+}
